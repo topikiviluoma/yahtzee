6da6667a6ed69e2067e44b58a2db0c93fe62fb51
Topi Kiviluoma <topikivi@dhcp-asv-113.helsinki.fi>
2016-04-07 12:54:50 +0300

    AddPlayers gui muokkailua

--- yahtzee/src/main/java/gui/AddPlayersListener.java
+++ yahtzee/src/main/java/gui/AddPlayersListener.java
@@ -7,6 +7,7 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JTextField;
 import yahtzee.logic.Player;
 
@@ -16,11 +17,14 @@ import yahtzee.logic.Player;
  */
 public class AddPlayersListener implements ActionListener {
     
-    
+    public String player1;
+    public String player2;
     private JTextField p1;
     private JTextField p2;
+    public ArrayList<String> players;
     
     public AddPlayersListener(JTextField nameField1, JTextField nameField2) {
+        this.players = new ArrayList<>();
         this.p1 = nameField1;
         this.p2 = nameField2;
        
@@ -29,8 +33,12 @@ public class AddPlayersListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Player player1 = new Player(p1.getText());
-        Player player2 = new Player(p2.getText());
+        player1 = p2.getText();
+        player2 = p2.getText();
+    }
+    
+    public ArrayList<String> getPlayers() {
+        return this.players;
     }
     
    
